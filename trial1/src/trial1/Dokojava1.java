8d049c4881fcc9c934e691bb51bd168df3d29c81
alronbleech <alron.bleech@gmail.com>
2023-09-26 18:06:59 +0900

    お試し

---
+++ trial1/src/trial1/Dokojava1.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package trial1;
+
+/**
+ * 
+ */
+public class Dokojava1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//変数（i）を表示する処理
+		for(int i = 0; i < 5; i++) {
+			System.out.println("---");
+			System.out.println(i);
+		}
+	}
+
+}
